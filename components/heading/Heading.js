1160f832b10749c513fe7c8ea7226eb5425dab36
Jessica <jessicakchell@gmail.com>
2022-02-02 10:39:59 +0100

    created heading

---
+++ components/heading/Heading.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const h1 = styled.h1`
+	font-family: 'Roboto';
+`;
+
+export default h1;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import GlobalStyle from '../styles/globals';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
