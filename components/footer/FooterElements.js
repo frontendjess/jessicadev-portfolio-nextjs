b4a6d5f11720b7d200451ada76d3bb267870a5f4
Jessica  Warr <jessica.warr@noaignite.com>
2023-06-01 11:54:33 +0200

    Update content, add files

--- components/footer/FooterElements.js
+++ components/footer/FooterElements.js
@@ -1,22 +1,23 @@
-import styled from 'styled-components';
-import Link from 'next/link';
-import media from 'styled-media-query';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
+import styled from "styled-components";
+import Link from "next/link";
+import media from "styled-media-query";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLongArrowAltUp } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const FooterContainer = styled.footer`
-	width: 100%;
+    width: 100%;
 `;
 
 const BuildMagicTogetherText = styled.h3`
-	font-family: var(--font-headings);
-	text-transform: uppercase;
-	font-size: var(--h3-size);
-	letter-spacing: var(--letter-spacing-xl);
-	line-height: var(--line-height-xl);
-	text-shadow: 2px 2px 2px var(--color-tertiary);
+    font-family: var(--font-headings);
+    text-transform: uppercase;
+    font-size: var(--h3-size);
+    letter-spacing: var(--letter-spacing-xl);
+    line-height: var(--line-height-xl);
+    text-shadow: 2px 2px 2px var(--color-tertiary);
 
-	${media.lessThan('medium')`
+    ${media.lessThan("medium")`
 		font-size: var(--h4-size);
 		letter-spacing: var(--letter-spacing-lg);
 		line-height: var(--line-height-lg);
@@ -24,178 +25,227 @@ const BuildMagicTogetherText = styled.h3`
 `;
 
 const IconArrow = styled.div`
-	display: flex;
-	justify-content: center;
+    display: flex;
+    justify-content: center;
 
-	${media.lessThan('medium')`
+    ${media.lessThan("medium")`
 		padding-left: 40px;
 	`}
 `;
 
 const BuildMagicTogether = () => {
-	return (
-		<div className='section-padding-top'>
-			<BuildMagicTogetherText>
-				Now that you know who I am, let&apos;s{' '}
-				<span className='secondary-text-color'>dream big</span> & build some
-				magic together
-			</BuildMagicTogetherText>
-			<IconArrow>
-				<img src='../images/icon-arrow-down-left.svg' alt='arrow' />
-			</IconArrow>
-		</div>
-	);
+    return (
+        <div className="section-padding-top">
+            <BuildMagicTogetherText>
+                Now that you know who I am, let&apos;s{" "}
+                <span className="secondary-text-color">dream big</span> & build
+                some magic together
+            </BuildMagicTogetherText>
+            <IconArrow>
+                <Image
+                    src="/images/icon-arrow-down-left.svg"
+                    alt="arrow"
+                    width={169}
+                    height={169}
+                />
+            </IconArrow>
+        </div>
+    );
 };
 
 const FooterRow = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-	${media.lessThan('medium')`
+    ${media.lessThan("medium")`
 	flex-direction: column;
 		flex-wrap: wrap;
 	`}
 `;
 
 const FooterCol = styled.div`
-	flex: 0 0 50%;
+    flex: 0 0 50%;
 
-	a {
-		font-family: var(--font-body);
-		font-size: var(--font-size-md);
-		letter-spacing: var(--letter-spacing-md);
-		font-weight: var(--font-weight-reg);
-	}
+    a {
+        font-family: var(--font-body);
+        font-size: var(--font-size-md);
+        letter-spacing: var(--letter-spacing-md);
+        font-weight: var(--font-weight-reg);
+    }
 `;
 
 const FooterAboutText = styled.div`
-	text-align: right;
-	font-family: var(--font-body);
-	font-size: var(--font-size);
-	letter-spacing: var(--letter-spacing-size);
-	line-height: var(--line-height-size);
+    text-align: right;
+    font-family: var(--font-body);
+    font-size: var(--font-size);
+    letter-spacing: var(--letter-spacing-size);
+    line-height: var(--line-height-size);
 
-	${media.lessThan('medium')`
+    ${media.lessThan("medium")`
 		padding-top: 50px;
 		text-align: left;
 	`}
 `;
 
 const FooterBtmCol = styled.div`
-	flex: 0 0 50%;
+    flex: 0 0 50%;
 
-	a {
-		font-family: var(--font-body);
-		font-size: var(--font-size);
-		letter-spacing: var(--letter-spacing-size);
-		font-weight: var(--font-weight-reg);
-		text-transform: uppercase;
-	}
+    a {
+        font-family: var(--font-body);
+        font-size: var(--font-size);
+        letter-spacing: var(--letter-spacing-size);
+        font-weight: var(--font-weight-reg);
+        text-transform: uppercase;
+    }
 
-	${media.lessThan('medium')`
+    ${media.lessThan("medium")`
 		padding-bottom: 25px;
 	`}
 `;
 
 const FooterCTA = () => {
-	return (
-		<div className='section-padding-btm'>
-			<FooterRow>
-				<FooterCol>
-					<div className='inner-content-padding-sm-btm'>
-						<Link href='mailto:hello@jessicadev.com'>
-							<a>EMAIL</a>
-						</Link>
-					</div>
-					<div className='inner-content-padding-sm-btm'>
-						<Link href='https://linkedin.com/in/jessica-dev'>
-							<a>LINKEDIN</a>
-						</Link>
-					</div>
-					<div>
-						<Link href='https://github.com/frontendjess'>
-							<a>GITHUB</a>
-						</Link>
-					</div>
-				</FooterCol>
-				<FooterCol>
-					<FooterAboutText>
-						<p className='italic'>Like what you see?</p>
-						<p>I designed my portfolio and built it on React & Next.js</p>
-					</FooterAboutText>
-				</FooterCol>
-			</FooterRow>
-		</div>
-	);
+    return (
+        <div className="section-padding-btm">
+            <FooterRow>
+                <FooterCol>
+                    <div className="inner-content-padding-sm-btm">
+                        <Link href="mailto:hello@jessicadev.com">
+                            <a>EMAIL</a>
+                        </Link>
+                    </div>
+                    <div className="inner-content-padding-sm-btm">
+                        <Link href="https://linkedin.com/in/jessica-dev">
+                            <a>LINKEDIN</a>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="https://github.com/frontendjess">
+                            <a>GITHUB</a>
+                        </Link>
+                    </div>
+                </FooterCol>
+                <FooterCol>
+                    <FooterAboutText>
+                        <p className="italic">Like what you see?</p>
+                        <p>
+                            I designed my portfolio and developed it in Next.js
+                            in Feb 2022
+                        </p>
+                    </FooterAboutText>
+                </FooterCol>
+            </FooterRow>
+        </div>
+    );
 };
 
 const FooterBottom = () => {
-	return (
-		<div className='inner-content-padding-btm'>
-			<FooterRow>
-				<FooterBtmCol>
-					<Link href='#Topofpage'>
-						<a>
-							Back to top <FontAwesomeIcon icon={faLongArrowAltUp} />
-						</a>
-					</Link>
-				</FooterBtmCol>
-				<FooterBtmCol>
-					<FooterAboutText>&copy; 2022</FooterAboutText>
-				</FooterBtmCol>
-			</FooterRow>
-		</div>
-	);
+    return (
+        <div className="inner-content-padding-btm">
+            <FooterRow>
+                <FooterBtmCol>
+                    <Link href="#Topofpage">
+                        <a>
+                            Back to top{" "}
+                            <FontAwesomeIcon icon={faLongArrowAltUp} />
+                        </a>
+                    </Link>
+                </FooterBtmCol>
+                <FooterBtmCol>
+                    <FooterAboutText>&copy; 2022</FooterAboutText>
+                </FooterBtmCol>
+            </FooterRow>
+        </div>
+    );
 };
 
 const WorkWithMe = () => {
-	return (
-		<>
-			<div className='marquee'>
-				<div className='marquee-wrapper'>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-					<div className='marquee-title'>
-						<span className='marquee-font'> WORK WITH ME </span>
-						<img src='../images/star.png' alt='star' />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="marquee">
+                <div className="marquee-wrapper">
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                    <div className="marquee-title">
+                        <span className="marquee-font"> WORK WITH ME </span>
+                        <Image
+                            src="/images/star.png"
+                            alt="star"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export {
-	FooterContainer,
-	BuildMagicTogether,
-	WorkWithMe,
-	FooterCTA,
-	FooterBottom,
+    FooterContainer,
+    BuildMagicTogether,
+    WorkWithMe,
+    FooterCTA,
+    FooterBottom,
 };
--- components/recentwork/RecentWork.js
+++ components/recentwork/RecentWork.js
@@ -1,109 +1,134 @@
-import styled from 'styled-components';
-import Link from 'next/link';
-import media from 'styled-media-query';
+import styled from "styled-components";
+import Link from "next/link";
+import media from "styled-media-query";
+import Image from "next/image";
 
 const RecentWorkTitle = () => {
-	return <p>SELECTED MOST RECENT WORK</p>;
+    return <p>SELECTED MOST RECENT WORK</p>;
 };
 
 const RecentWorkDetails = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: column;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Section = styled.section`
-	padding: 100px 0;
+    padding: 100px 0;
 
-	${media.lessThan('large')`
+    ${media.lessThan("large")`
 		padding: 200px 0 100px 0;
 	`}
 `;
 
 const RecentWork = () => {
-	return (
-		<>
-			<Section>
-				<RecentWorkTitle />
-				<div className='inner-content-padding'>
-					<RecentWorkDetails>
-						<img
-							className='mockup-image'
-							src='../images/selectedworks/restaurant-mockup.png'
-							alt='client work mockup'
-						/>
+    return (
+        <>
+            <Section>
+                <RecentWorkTitle />
+                <div className="inner-content-padding">
+                    <RecentWorkDetails>
+                        <Image
+                            className="mockup-image"
+                            src="/images/selectedworks/noanorge-mockup.png"
+                            alt="Noanorge client work mockup"
+                            width={1728}
+                            height={1117}
+                        />
 
-						<Link href='/projects/1'>
-							<a className='project-link'>
-								01. <span className='project-type-text'>[ Design ]</span>{' '}
-								Restaurant Design Project
-							</a>
-						</Link>
-					</RecentWorkDetails>
-				</div>
+                        <Link href="/projects/1">
+                            <a className="project-link">
+                                01.{" "}
+                                <span className="project-type-text">
+                                    [ Development ]
+                                </span>{" "}
+                                Noanorge.no - Next.js + Sanity + Tailwind
+                                website
+                            </a>
+                        </Link>
+                    </RecentWorkDetails>
+                </div>
 
-				<div className='inner-content-padding'>
-					<RecentWorkDetails>
-						<img
-							className='mockup-image'
-							src='../images/selectedworks/tgp-mockup.png'
-							alt='Client work mockup'
-						/>
+                <div className="inner-content-padding">
+                    <RecentWorkDetails>
+                        <Image
+                            className="mockup-image"
+                            src="/images/selectedworks/oyaxfretex-mockup.png"
+                            alt="Client work mockup"
+                            width={1728}
+                            height={1117}
+                        />
 
-						<Link href='/projects/2'>
-							<a className='project-link'>
-								02. <span className='project-type-text'>[ Design + Dev]</span>{' '}
-								The Garden Party - Designs & Productions Studio Website
-							</a>
-						</Link>
-					</RecentWorkDetails>
-				</div>
+                        <Link href="/projects/4">
+                            <a className="project-link">
+                                02.{" "}
+                                <span className="project-type-text">
+                                    [ Development ]
+                                </span>{" "}
+                                Øya X Fretex - Next.js + Tailwind + Framer
+                                Motion Website
+                            </a>
+                        </Link>
+                    </RecentWorkDetails>
+                </div>
 
-				<div className='inner-content-padding'>
-					<RecentWorkDetails>
-						<img
-							className='mockup-image'
-							src='../images/selectedworks/interiorshop-mockup.png'
-							alt='Ecommerce work mockup'
-						/>
+                <div className="inner-content-padding">
+                    <RecentWorkDetails>
+                        <Image
+                            className="mockup-image"
+                            src="/images/selectedworks/tgp-mockup.png"
+                            alt="Client work mockup"
+                            width={1728}
+                            height={1117}
+                        />
 
-						<Link href='/projects/4'>
-							<a className='project-link'>
-								03. <span className='project-type-text'>[ Design + Dev ]</span>{' '}
-								E-commerce Interior Webshop
-							</a>
-						</Link>
-					</RecentWorkDetails>
-				</div>
+                        <Link href="/projects/4">
+                            <a className="project-link">
+                                02.{" "}
+                                <span className="project-type-text">
+                                    [ Design + Dev]
+                                </span>{" "}
+                                The Garden Party - Designs & Productions Studio
+                                Website
+                            </a>
+                        </Link>
+                    </RecentWorkDetails>
+                </div>
 
-				<div className='inner-content-padding-btm projects-row'>
-					<div className='projects-col'>
-						<div className='inner-content-padding-btm'>
-							<p>WANT TO SEE MORE?</p>
-						</div>
-						<div className='inner-content-padding-btm'>
-							<Link href='/projects'>
-								<a className='projects-text-link-secondary'>
-									View all works and projects
-								</a>
-							</Link>
-						</div>
+                <div className="inner-content-padding-btm projects-row">
+                    <div className="projects-col">
+                        <div className="inner-content-padding-btm">
+                            <p>WANT TO SEE MORE?</p>
+                        </div>
+                        <div className="inner-content-padding-btm">
+                            <Link href="/projects">
+                                <a className="projects-text-link-secondary">
+                                    View all works and projects
+                                </a>
+                            </Link>
+                        </div>
 
-						<div className='inner-content-padding-btm'>
-							<a
-								className='projects-text-link'
-								href='mailto:hello@jessicadev.com'>
-								Or get in touch with me
-							</a>
-						</div>
-					</div>
-					<div className='projects-col projects-col-star'>
-						<img src='../images/star.png' alt='star graphic' />
-					</div>
-				</div>
-			</Section>
-		</>
-	);
+                        <div className="inner-content-padding-btm">
+                            <a
+                                className="projects-text-link"
+                                href="mailto:hello@jessicadev.com"
+                            >
+                                Or get in touch with me
+                            </a>
+                        </div>
+                    </div>
+                    <div className="projects-col projects-col-star">
+                        <Image
+                            src="/images/star.png"
+                            alt="star graphic"
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+                </div>
+            </Section>
+        </>
+    );
 };
 
 export default RecentWork;
